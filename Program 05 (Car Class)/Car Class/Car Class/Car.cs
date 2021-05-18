548cd5991b128a3120c9156f770e7b6e0d9e5fc9
DapperProgrammer <74994236+DapperProgrammer@users.noreply.github.com>
2021-05-18 11:35:23 -0500

    Add files via upload

---
+++ Program 05 (Car Class)/Car Class/Car Class/Car.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Class
+{
+    class Car
+    {
+        // Fields
+
+        private string _year;
+        private string _make;
+        private int _speed;
+
+        // Constructor, initalizies values
+        public Car()
+        {
+            _year = "";
+            _make = "";
+            _speed = 0;
+        }
+
+        // Year Method/Property
+        // The value parameter is automatically created by the compiler.
+        // It also sets itself as the same data type that the method/property are.
+        // Since the compiler automatically creates the "value" parameter
+        // we do not need to declare/initalize it. 
+        public string GetYear
+        {
+            set { _year = value; }
+            get { return _year;  }
+        }
+
+        // Make Method/Property
+        public string GetMake
+        {
+            set { _make = value; }
+            get { return _make;  }
+        }
+
+        // Speed Method/Property
+        // Always initalized to 0
+        // The "value" and therefore the "_speed" property are always initalized 0 when the accelerate button is hit
+        // As that is what was initalized above and thus never changes. 
+        // However at the end of the calculations from Accelerate "GetSpeed" and it's "_speed" property are equal to the reault
+        // of said calculations. But are then re initalized to 0 after the accelerate button is hit again. 
+        public int GetSpeed
+        {
+            set { _speed = value; }
+            get { return _speed; }
+        }
+
+        public int Accelerate(int spdResult)
+        {
+            // Every iteration of the loop "GetSpeed" is always equal to 0
+            // For example:
+            // GetSpeed = 0 + 5 + 0 = 0
+            // GetSpeed = 0 + 5 + 5 = 10
+            // GetSpeed = 0 + 5 + 10 = 15
+            // And so on. 
+            GetSpeed = GetSpeed + 5 + spdResult;
+            return GetSpeed;
+        }
+
+        public int Break(int spdResult)
+        {
+            //GetSpeed = GetSpeed - 5 + spdResult;
+            GetSpeed = spdResult - 5 + GetSpeed;
+            return GetSpeed;
+
+        }
+
+    }
+}
---
+++ Program 05 (Car Class)/Car Class/Car Class/MainForm.cs
@@ -0,0 +1,84 @@
+﻿// Calvin Tracy
+// CS 313 01
+// Program Assignment 05
+// 20200401
+// Program to accelerate and deceletate the spd of a car
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Car_Class
+{
+    public partial class MainForm : Form
+    {
+        // Create a Car object.
+        Car myCar = new Car();
+        int result = 0;
+        int brkResult;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // Sets the values of the fields for the Car Class
+        private void GetCarData()
+        {
+            try
+            {
+                myCar.GetYear = txtYear.Text; 
+                myCar.GetMake = txtMake.Text;
+                myCar.GetSpeed = 0;
+            }
+            catch
+            {
+                MessageBox.Show("Please enter a valid year and model.");
+            }
+            //lbCar.Items.Add(specs.GetYear);
+            //lbCar.Items.Add(specs.GetMake);
+        }
+
+        private void btnAccel_Click(object sender, EventArgs e)
+        {
+
+            GetCarData();
+            // Passes in "result" to the accelerate method. 
+            myCar.Accelerate(result);
+            txtSpeed.Text = myCar.GetSpeed.ToString();
+            result = myCar.GetSpeed;
+
+            if (result == 100)
+            {
+                MessageBox.Show("Woah to fast!");
+                btnAccel.Enabled = false;
+            }
+            
+        }
+
+        private void btnBreak_Click(object sender, EventArgs e)
+        {
+            GetCarData();
+            myCar.Break(result);
+            txtSpeed.Text = myCar.GetSpeed.ToString();
+            brkResult = myCar.GetSpeed;
+
+            if (brkResult <= 0)
+            {
+                MessageBox.Show("Your car isn't moving!");
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
